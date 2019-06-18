a505dd8648b2c0760f51a39b44fbc586780b1075
mmarkelov <maks-markel@mail.ru>
2019-06-18 15:40:59 +0300

    Updates

---
+++ precache-manifest.57ab407548a38eab8d70c9c7c6a802ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2380f6334831e0d399e9",
+    "url": "/vk-apps-emoji-learn/static/js/runtime~main.27cc8d64.js"
+  },
+  {
+    "revision": "03caab753f3cc91da885",
+    "url": "/vk-apps-emoji-learn/static/js/main.49e4e69b.chunk.js"
+  },
+  {
+    "revision": "ea239704c95b3ff9fcc7",
+    "url": "/vk-apps-emoji-learn/static/js/2.966f4d22.chunk.js"
+  },
+  {
+    "revision": "03caab753f3cc91da885",
+    "url": "/vk-apps-emoji-learn/static/css/main.658b5bb9.chunk.css"
+  },
+  {
+    "revision": "ea239704c95b3ff9fcc7",
+    "url": "/vk-apps-emoji-learn/static/css/2.3738f771.chunk.css"
+  },
+  {
+    "revision": "78f462d252a689b0bfa0df8d6dcb3349",
+    "url": "/vk-apps-emoji-learn/index.html"
+  }
+];
